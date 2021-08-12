bbb9d696eadea20f84629b51c70e775d17871b56
mayurilamkane <47909812+mayurilamkane@users.noreply.github.com>
2021-08-12 12:31:01 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,13 @@
+# add implementation
+def add(x,y):
+    pass
+# substract implentaed
+def subtract(x,y):
+    return x-y   #on master branch
+#multiply implementation
+def multiply(x,y):
+    return x*y #on Bug456
+def divide(x,y):
+    pass
+def square(x):
+	pass
